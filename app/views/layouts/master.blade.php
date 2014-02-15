0f1c9163fcb6f8afe6e3677aea266ac873f455a0
jarstingstall <jrarst01@gmail.com>
2014-02-15 17:58:55 -0500

    finish posts.create view

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
 	</head>
 	<body>
-	   @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
 	</body>
 </html>
--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
+    <h2>Create New Post</h2>
     {{ $app['form']->open(['route' => 'posts.index']) }}
+
+    <p>
+        {{ $app['form']->label('title') }}
+        {{ $app['form']->text('title') }}
+    </p>
+    <p>
+        {{ $app['form']->label('body') }}
+        {{ $app['form']->textarea('body') }}
+    </p>
+
+        {{ $app['form']->submit() }}
 
     {{ $app['form']->close() }}
 @stop
