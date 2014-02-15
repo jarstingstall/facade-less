4c95bf4249a132be616205bcec55ae7fe0c7b03d
jarstingstall <jrarst01@gmail.com>
2014-02-15 17:49:55 -0500

    remove Acme dir

--- app/Acme/Repositories/ArticleRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Acme\Repositories;
-
-interface ArticleRepositoryInterface
-{
-	public function byPage($page = 1, $limit = 10);
-	public function bySlug($slug);
-	public function byTag($tag, $page = 1, $limit = 10);
-}
